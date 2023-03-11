77c9d582b2a1a973e25137d4cda35c4691bc0cf4
ayanokoujifl <guleite3@gmail.com>
2023-03-11 19:25:42 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { client } from './lib/apollo'
 import { Event } from './pages/Event'
 import { Router } from './Router'
 import './styles/global.css'
+
 function App() {
   return (
     <ApolloProvider client={client}>
